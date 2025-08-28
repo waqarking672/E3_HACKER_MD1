a77c63ca48f4d6715004cc6439df163b4553789a
ALUSTADYT <115578168+ALUSTADYT@users.noreply.github.com>
2025-08-28 09:37:37 +0500

    Update check-update.js

--- plugins/check-update.js
+++ plugins/check-update.js
@@ -16,24 +16,24 @@ cmd({
     if (!isOwner) return reply("This command is only for the bot owner.");
 
     try {
-        await reply("🔍 Checking for HASI-MD updates...");
+        await reply("🔍 Checking for E3_HACKER_MD updates...");
 
         // Fetch the latest commit hash from GitHub
-        const { data: commitData } = await axios.get("https://api.github.com/repos/Arslan-MD/Arslan_MD/commits/main");
+        const { data: commitData } = await axios.get("https://api.github.com/repos/ALUSTADYT/E3_HACKER_MD/commits/main");
         const latestCommitHash = commitData.sha;
 
         // Get the stored commit hash from the database
         const currentHash = await getCommitHash();
 
         if (latestCommitHash === currentHash) {
-            return reply("✅ Your Arslan_MD bot is already up-to-date!");
+            return reply("✅ Your E3_HACKER_MD bot is already up-to-date!");
         }
 
-        await reply("🚀 Updating Arslan_MD Bot...");
+        await reply("🚀 Updating E3_HACKER_MD Bot...");
 
         // Download the latest code
         const zipPath = path.join(__dirname, "latest.zip");
-        const { data: zipData } = await axios.get("https://github.com/Arslan-MD/Arslan_MD/archive/main.zip", { responseType: "arraybuffer" });
+        const { data: zipData } = await axios.get("https://github.com/ALUSTADYT/E3_HACKER_MD/archive/main.zip", { responseType: "arraybuffer" });
         fs.writeFileSync(zipPath, zipData);
 
         // Extract ZIP file
@@ -44,7 +44,7 @@ cmd({
 
         // Copy updated files, preserving config.js and app.json
         await reply("🔄 Replacing files...");
-        const sourcePath = path.join(extractPath, "Arslan_MD-main");
+        const sourcePath = path.join(extractPath, "E3_HACKER_MD-main");
         const destinationPath = path.join(__dirname, '..');
         copyFolderSync(sourcePath, destinationPath);
 
